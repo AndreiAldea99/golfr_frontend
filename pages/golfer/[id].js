1fe2ba09685f0eb37736bb9487d414c5e0825486
Andrei Aldea <andrei.aldea@golfgenius.com>
2022-07-26 15:22:02 +0300

    Moved the data fetching logic into two new files

--- pages/golfer/[id].js
+++ pages/golfer/[id].js
@@ -1,76 +1,26 @@
 import { useRouter } from 'next/router'
-// import useUser from '../../lib/useUser'
 import ScoreCard from '../../components/ScoreCard'
-import { useEffect, useState } from 'react'
-
-const FEED_URL = `${process.env.NEXT_PUBLIC_API_URL}/golfer`
+import useGolferName from '../../lib/useGolferName'
+import useGolferScores from '../../lib/useGolferScores'
 
 const Golfer = () => {
   const router = useRouter()
   const { id } = router.query
 
-  const [ username, setUsername ] = useState(null)
-  const [ scores, setScores ] = useState(null)
-
-  useEffect(() => {
-    const urlName = FEED_URL + '/' + id + '/name'
-    const urlScores = FEED_URL + '/' + id + '/scores'
-
-    const fetchDataName = async () => {
-      try {
-        const response = await fetch(urlName)
-
-        if (!response.ok) {
-          return response.text().then(text => {
-            throw new Error(text)
-          })
-        }
-
-        const json = await response.json()
-
-        setUsername(json.user.name)
-
-      } catch (error) {
-        throw new Error(error)
-      }
-    }
-
-    const fetchDataScores = async () => {
-      try {
-        const response = await fetch(urlScores)
-
-        if (!response.ok) {
-          return response.text().then(text => {
-            throw new Error(text)
-          })
-        }
-
-        const json = await response.json()
-
-        setScores(json.user.scores)
-
-      } catch (error) {
-        throw new Error(error)
-      }
-    }
-
-    if (router.isReady) {
-      fetchDataName()
-      fetchDataScores()
-    }
-  }, [ router.isReady, id ])
+  const { golferName, errorGolfer } = useGolferName(id)
+  const { golferScores, errorScores }  = useGolferScores(id)
 
   return (
     <>
       <header className="flex flex-row w-full px-10 py-2 shadow">
         <span className="h-8 items-center text-xl ml-auto">
-          {username}
+          {golferName}
         </span>
       </header>
       {
         <>
-          {scores &&
-            scores.map(score => (
+          {golferScores &&
+            golferScores.map(score => (
               <ScoreCard
                 key={score.id}
                 id={score.id}
